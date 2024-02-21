6f97ea70345c701ac2678f07d96dfa1d75cd8b7b
Daniel Tressel <dhtressel@gmail.com>
2024-02-21 10:10:18 -0500

    clean up footer

--- src/layouts/Footer/Footer.js
+++ src/layouts/Footer/Footer.js
@@ -1,11 +1,8 @@
-import { NavLink } from "react-router-dom";
 import './Footer.css';
 
 const Footer = () => {
   return (
     <div className="Footer">
-      <div className="Footer-links"></div>
-      <div className="Footer-social-links"></div>
       <div className="Footer-copyright">
         <div>&copy; 2024 Daniel Tressel</div>
       </div>
