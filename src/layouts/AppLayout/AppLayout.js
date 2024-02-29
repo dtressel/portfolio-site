a9780cdeef6ccd309a0d2a3137d3ed8a70a4a2cd
Daniel Tressel <dhtressel@gmail.com>
2024-02-28 23:04:15 -0500

    created technologies layout component

--- src/layouts/AppLayout/AppLayout.js
+++ src/layouts/AppLayout/AppLayout.js
@@ -1,14 +1,22 @@
 import NavBar from "../NavBar";
 import Footer from "../Footer";
 import BottomNavigation from "../BottomNavigation";
-import { Outlet } from "react-router-dom";
+import Technologies from "../Technologies";
+import { Outlet, useLocation } from "react-router-dom";
 import './AppLayout.css';
 
 const AppLayout = () => {
+  const location = useLocation();
+  let includeTechnologies = false;
+  if (['/experience', '/projects'].includes(location.pathname)) {
+    includeTechnologies = true;
+  }
+
   return (
     <div className="page-container">
       <NavBar />
       <main>
+        {includeTechnologies && <Technologies />}
         <Outlet />
       </main>
       <BottomNavigation />
---
+++ src/layouts/Technologies/Technologies.js
@@ -0,0 +1,11 @@
+import './Technologies.css';
+
+const Technologies = () => {
+  return (
+    <div className="Technologies">
+      These are technologies
+    </div>
+  )
+}
+
+export default Technologies;
